d972725ee534ebef034b7868701112984c046e84
Anthony Alvarez <velurn123@gmail.com>
2025-05-14 23:01:01 -0600

    Fix formatting in ApiException and UserNotFoundException by removing unnecessary blank lines

--- bookclub/src/main/java/com/portfolio/bookclub/bookclub/service/exception/UserNotFoundException.java
+++ bookclub/src/main/java/com/portfolio/bookclub/bookclub/service/exception/UserNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends ApiException{
-
+        
     public UserNotFoundException(Long id){
         super("User Not found with the Id " + id, "USER_NOT_FOUND");
     }
