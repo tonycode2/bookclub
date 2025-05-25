48245c1bd5c1c7600f03489a908dbb72a0986200
Anthony Alvarez <velurn123@gmail.com>
2025-05-24 21:08:19 -0600

    feat: adding ClubByUserImpl to the project. The only thing it suposes to do is to be an intermediate table between club and user

--- bookclub/src/main/java/com/portfolio/bookclub/bookclub/persistance/repository/ClubByUserRepo.java
+++ bookclub/src/main/java/com/portfolio/bookclub/bookclub/persistance/repository/ClubByUserRepo.java
@@ -1,5 +1,7 @@
 package com.portfolio.bookclub.bookclub.persistance.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.portfolio.bookclub.bookclub.persistance.entity.ClubByUser;
 
 @Repository
 public interface ClubByUserRepo extends JpaRepository<ClubByUser, Long>{
-
+    Optional<ClubByUser> findByUserIdAndClubId(Integer userId, Long clubId);
 }
--- bookclub/src/main/java/com/portfolio/bookclub/bookclub/presentation/dto/ClubByUserDto.java
+++ bookclub/src/main/java/com/portfolio/bookclub/bookclub/presentation/dto/ClubByUserDto.java
@@ -2,11 +2,15 @@ package com.portfolio.bookclub.bookclub.presentation.dto;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClubByUserDto {
     
     Long id;
