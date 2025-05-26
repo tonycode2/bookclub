c14c4931989227e3f5ff4deda8cdc7c5a2cf350b
Anthony Alvarez <velurn123@gmail.com>
2025-05-26 16:54:06 -0600

    feat: adding all controllers

--- bookclub/src/main/java/com/portfolio/bookclub/bookclub/presentation/controller/ClubByUserController.java
+++ bookclub/src/main/java/com/portfolio/bookclub/bookclub/presentation/controller/ClubByUserController.java
@@ -1,5 +1,35 @@
 package com.portfolio.bookclub.bookclub.presentation.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.portfolio.bookclub.bookclub.presentation.dto.ClubByUserDto;
+import com.portfolio.bookclub.bookclub.service.interfaces.ClubByUserService;
+
+@RestController
+@RequestMapping("/api/v1/club-by-user")
 public class ClubByUserController {
+
+    private final ClubByUserService service;
+    public ClubByUserController(ClubByUserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/{userId}/{clubId}")
+    public ResponseEntity<ClubByUserDto> create(@PathVariable Integer userId, @PathVariable Long clubId) {
+        ClubByUserDto created = service.create(userId, clubId);
+        return ResponseEntity.ok(created);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@PathVariable Integer userId, @PathVariable Long clubId) {
+        service.delete(userId, clubId);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- bookclub/src/main/java/com/portfolio/bookclub/bookclub/presentation/controller/ReviewController.java
+++ bookclub/src/main/java/com/portfolio/bookclub/bookclub/presentation/controller/ReviewController.java
@@ -1,5 +1,37 @@
 package com.portfolio.bookclub.bookclub.presentation.controller;
 
-public class ReviewController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.portfolio.bookclub.bookclub.presentation.dto.ReviewCreateDto;
+import com.portfolio.bookclub.bookclub.presentation.dto.ReviewDto;
+import com.portfolio.bookclub.bookclub.service.interfaces.ReviewService;
+
+@RequestMapping("/api/v1/review")
+@RestController
+public class ReviewController {
+    private final ReviewService reviewService;
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReviewDto> createReview(@RequestBody ReviewCreateDto reviewCreateDto) {
+        ReviewDto createdReview = reviewService.create(reviewCreateDto);
+        return ResponseEntity.ok(createdReview);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<ReviewDto> updateReview(@RequestBody ReviewDto reviewDto) {
+        ReviewDto updatedReview = reviewService.update(reviewDto);
+        return ResponseEntity.ok(updatedReview);
+    }
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteReview(@RequestBody Long id) {
+        reviewService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
