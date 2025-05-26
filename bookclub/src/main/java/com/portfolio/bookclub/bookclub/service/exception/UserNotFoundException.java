61ca5167a13c462c25de83ee4f342a666135aa8d
Anthony Alvarez <velurn123@gmail.com>
2025-05-25 18:38:08 -0600

    feat: attempting to resolve conflicts

--- bookclub/src/main/java/com/portfolio/bookclub/bookclub/service/exception/UserNotFoundException.java
+++ bookclub/src/main/java/com/portfolio/bookclub/bookclub/service/exception/UserNotFoundException.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends ApiException{
-
     public UserNotFoundException(Integer id){
         super("User Not found with the Id " + id, "USER_NOT_FOUND");
     }
-
 }
